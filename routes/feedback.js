15f18b47ce9b1b3d71960d9fca8c7a63566393ff
Thounda Craig <thoundac94@gmail.com>
2023-02-12 17:38:03 -0500

    Create a EJS form template

--- routes/feedback.js
+++ routes/feedback.js
@@ -2,21 +2,23 @@ const express = require('express');
 
 const router = express.Router();
 
-module.exports = params => {
+module.exports = (params) => {
   const { feedbackService } = params;
 
   router.get('/', async (request, response, next) => {
     try {
       const feedback = await feedbackService.getList();
-      return response.json(feedback);
+      return response.render('layout', {
+        pageTitle: 'Feedback',
+        template: 'feedback',
+        feedback,
+      });
     } catch (err) {
       return next(err);
     }
   });
 
-  router.post('/', (request, response) => {
-    return response.send('Feedback form posted');
-  });
+  router.post('/', (request, response) => response.send('Feedback form posted'));
 
   return router;
 };
